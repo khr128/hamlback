17c708fb4bc61eb1077abcbb9905b13595eb8329
khr <abusik@hotmail.com>
2010-04-23 16:03:07 -0700

    Refactored append(), added concatenate() and strtrim().

--- src/haml_helpers.h
+++ src/haml_helpers.h
@@ -4,26 +4,50 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 char *append(char *dst, char *src)
 {
-  char *text;
-  if(dst)
-  {
-    text = (char *)calloc(strlen(dst)+strlen(src)+1, 1); 
-    strcpy(text, dst); 
-    strcat(text, src);
-    free(dst);
-  }
-  else
-  {
-    text = (char *)calloc(strlen(src)+1, 1); 
-    strcpy(text, src);
-  }
+  if(!src) return dst;
+  if(!dst) return strdup(src);
 
-  dst = text;
+  char *text;
+  text = (char *)calloc(strlen(dst)+strlen(src)+1, 1); 
+  strcpy(text, dst); 
+  strcat(text, src);
+
+  free(dst);
   return text;
 }
 
+char *concatenate(int n, ...)
+{
+  va_list strings;
+  va_start(strings, n);
 
+  char *concatenated_str = strdup(va_arg(strings, char *));
+  int i;
+  for (i=0; i<n-1; ++i)
+    concatenated_str = append(concatenated_str, va_arg(strings, char *));
+
+  va_end(strings);
+
+  return concatenated_str;
+}
+
+char *strtrim(char *str, char c)
+{
+  size_t n = strlen(str);
+  char *trimmed = strdup(str);
+
+  if(n < 1) return trimmed;
+    
+  if(*str == c) memcpy(trimmed, trimmed+1, n);
+
+  if(n < 2) return trimmed;
+
+  if(*(str+n-1) == c) *(trimmed+strlen(trimmed)-1) = '\0';
+
+  return trimmed;
+}
 #endif /*HAML_HELPERS_H_*/
