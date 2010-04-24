817fd1f066c0ab2fd414cf93ef53b8abcd2e48a1
khr <abusik@hotmail.com>
2010-04-24 11:46:05 -0700

    Introduced HAML_STACk struct to be able to use stack with complex data.

--- src/haml_stack.h
+++ src/haml_stack.h
@@ -2,19 +2,38 @@
 #define HAML_STACK_H
 
 #define TAG_NAME_STACK_SIZE 1024
-int tag_name_sp = -1; /* empty stack */
-char *tag_name_stack[TAG_NAME_STACK_SIZE];
+#define HAML_STACK_SIZE 1024
+int haml_sp = -1; /* empty stack */
 
-char *pop_name()
+struct HAML_STACK {
+  char *tag_name;
+};
+
+struct HAML_STACK haml_null = { 0 };
+struct HAML_STACK haml_stack = { 0 };
+
+struct HAML_STACK stack[HAML_STACK_SIZE];
+
+struct HAML_STACK haml_pop()
 {
-  if(tag_name_sp < 0) return 0;
-  return tag_name_stack[tag_name_sp--];
+  if(haml_sp < 0) return haml_null;
+  return stack[haml_sp--];
 }
 
-void push_name(char *name)
+void haml_push(struct HAML_STACK stack_element)
 {
-  if(tag_name_sp == TAG_NAME_STACK_SIZE) return;
-  tag_name_stack[++tag_name_sp] = name;
+  if(haml_sp == HAML_STACK_SIZE) return;
+  stack[++haml_sp] = stack_element;
+}
+
+int haml_cmp(struct HAML_STACK el1, struct HAML_STACK el2)
+{
+  return el1.tag_name == el2.tag_name;
+}
+
+void haml_clean(struct HAML_STACK el)
+{
+  free(el.tag_name);
 }
 
 #endif /*HAML_STACK_H*/
