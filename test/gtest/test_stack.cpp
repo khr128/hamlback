8b01915e5e44d32f47eb8cc3ad61be574ff68881
khr <abusik@hotmail.com>
2010-04-26 10:39:26 -0700

    Added basic gtest files for stack testing.

---
+++ test/gtest/test_stack.cpp
@@ -0,0 +1,13 @@
+#include "test_stack.h"
+#include <haml_stack.h>
+
+void StackTests::SetUp()
+{}
+
+void StackTests::TearDown()
+{}
+
+TEST_F(StackTests, InstantiationTest) 
+{
+}
+
---
+++ test/gtest/test_stack.h
@@ -0,0 +1,14 @@
+#ifndef gtest_stack_H_
+#define gtest_stack_H_
+#include <gtest/gtest.h>
+
+class StackTests : public ::testing::Test
+{
+
+public:
+  virtual void SetUp();
+  virtual void TearDown();
+
+};
+#endif //gtest_stack_H_
+
