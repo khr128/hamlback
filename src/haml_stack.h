aac8f59a213136b1bb51cccb320aada35dad1352
khr <abusik@hotmail.com>
2010-04-27 17:56:50 -0700

    1. Added inden string to stack element. 2. Added haml_peek() function to examine the top of stack without popping it.

--- src/haml_stack.h
+++ src/haml_stack.h
@@ -7,12 +7,19 @@ int haml_sp = -1; /* empty stack */
 
 struct HAML_STACK {
   char *tag_name;
+  char *indent;
 };
 
-struct HAML_STACK haml_null = { 0 };
-struct HAML_STACK haml_stack = { 0 };
+struct HAML_STACK haml_null = { 0, 0 };
+struct HAML_STACK haml_stack = { 0, 0 };
 
 struct HAML_STACK stack[HAML_STACK_SIZE];
+
+struct HAML_STACK haml_peek()
+{
+  if(haml_sp < 0) return haml_null;
+  return stack[haml_sp];
+}
 
 struct HAML_STACK haml_pop()
 {
@@ -28,13 +35,17 @@ void haml_push(struct HAML_STACK stack_element)
 
 int haml_cmp(struct HAML_STACK el1, struct HAML_STACK el2)
 {
-  return el1.tag_name == el2.tag_name;
+  return el1.tag_name == el2.tag_name &&
+    el1.indent == el2.indent;
 }
 
 void haml_clean(struct HAML_STACK *el)
 {
   free(el->tag_name);
+  free(el->indent);
+
   el->tag_name = 0;
+  el->indent = 0;
 }
 
 #endif /*HAML_STACK_H*/
