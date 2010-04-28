d4fc85a9877bb6f9c3e77a4b9ef80d82c2060a88
khr <abusik@hotmail.com>
2010-04-27 17:52:37 -0700

    Added close_previously_open_tags() helper.

--- src/haml_helpers.h
+++ src/haml_helpers.h
@@ -7,4 +7,16 @@
 #include "haml_stack.h"
 #include "haml_indent.h"
 
+void close_previously_parsed_tags()
+{
+  /*close previously parsed tags if necessary*/
+  haml_stack = haml_peek();
+  while(!haml_cmp(haml_stack, haml_null) 
+      && strlen(haml_stack.indent) >= haml_current_indent )
+  {
+    haml_stack = haml_pop();
+    printf ("%s</%s>\n", haml_stack.indent, haml_stack.tag_name);  
+    haml_stack = haml_peek();
+  }
+}
 #endif /*HAML_HELPERS_H_*/
