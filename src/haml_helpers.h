a2900e6e655bf4bbe53577ad0866c73f33472df0
khr <abusik@hotmail.com>
2010-04-29 15:25:18 -0700

    Added push_tag_name and make_tag_name helpers.

--- src/haml_helpers.h
+++ src/haml_helpers.h
@@ -32,4 +32,33 @@ void haml_free(int n, ...)
   va_end(ptrs);
 }
 
+void push_tag_name(char *name, char *indent)
+{
+  close_previously_parsed_tags();
+  haml_stack.tag_name = strdup(name);
+  haml_stack.indent = strdup(indent);
+  haml_push(haml_stack);
+}
+
+char *make_tag_name(char* indent, char *name)
+{
+  int indent_length = strlen(indent);
+  if(indent_length == 0)
+  {
+    haml_current_indent = 0;
+  }
+  else
+  {
+    if(!haml_set_space_indent(indent_length))
+      return 0;
+  }
+
+  push_tag_name(strdup(name), strdup(indent));
+
+  char *tag_name = concatenate(3, indent, "<", name);
+  haml_free(2, indent, name);
+
+  return tag_name;
+}
+
 #endif /*HAML_HELPERS_H_*/
