3d471ababd9c6f46d8411ad8f82a60c45e88f698
khr <abusik@hotmail.com>
2010-05-01 19:30:36 -0700

    1. Used deep copy to store stack elements 2. Added haml_execute_stack(). 3. Removed extern access to global vars. Removed haml_stack var altogether.

--- src/haml_stack.c
+++ src/haml_stack.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
+#include <string.h>
 #include "haml_stack.h"
 int haml_sp = -1; /* empty stack */
 
 struct HAML_STACK haml_null = { 0, 0 };
-struct HAML_STACK haml_stack = { 0, 0 };
+/*struct HAML_STACK haml_stack = { 0, 0 };*/
+
+#define HAML_STACK_SIZE 1024
 struct HAML_STACK stack[HAML_STACK_SIZE];
+
+int haml_stack_pointer() {return haml_sp;}
 
 struct HAML_STACK haml_peek()
 {
@@ -21,20 +26,50 @@ struct HAML_STACK haml_pop()
 void haml_push(struct HAML_STACK stack_element)
 {
   if(haml_sp == HAML_STACK_SIZE) return;
-  stack[++haml_sp] = stack_element;
+  haml_deep_copy(&stack[++haml_sp], &stack_element);
 }
 
 int haml_cmp(struct HAML_STACK el1, struct HAML_STACK el2)
 {
-  return el1.tag_name == el2.tag_name &&
-    el1.indent == el2.indent;
+  if(
+      el1.tag_name == 0 && el2.tag_name != 0 ||
+      el1.tag_name != 0 && el2.tag_name == 0 ||
+      el1.indent == 0 && el2.indent != 0 ||
+      el1.indent != 0 && el2.indent == 0
+    ) return 0;
+  else if (
+      el1.tag_name == 0 && el2.tag_name == 0 &&
+      el1.tag_name == 0 && el2.tag_name == 0
+      ) return 1;
+  else 
+    return strcmp(el1.tag_name, el2.tag_name) == 0 &&
+           strcmp(el1.indent, el2.indent) == 0;
 }
 
 void haml_clean(struct HAML_STACK *el)
 {
-  free(el->tag_name);
-  free(el->indent);
+  if(!el) return;
+  if(el->tag_name) free(el->tag_name);
+  if(el->indent) free(el->indent);
 
   el->tag_name = 0;
   el->indent = 0;
+}
+
+void haml_deep_copy(struct HAML_STACK *dst, struct HAML_STACK *src)
+{
+  dst->tag_name = src->tag_name ? strdup(src->tag_name) : 0;
+  dst->indent = src->indent ? strdup(src->indent) : 0;
+}
+
+void haml_execute_stack(stack_op f)
+{
+  /*call stack_op for each element, and empty and clean stack*/
+  struct HAML_STACK el = haml_pop();
+  while(!haml_cmp(el, haml_null))
+  {
+    if(f) f(&el);
+    haml_clean(&el);
+    el = haml_pop();
+  }
 }
--- src/haml_stack.h
+++ src/haml_stack.h
@@ -5,25 +5,23 @@
  extern "C" {
 #endif
 
-#define TAG_NAME_STACK_SIZE 1024
-#define HAML_STACK_SIZE 1024
-extern int haml_sp; /* stack pointer */
-
 struct HAML_STACK {
   char *tag_name;
   char *indent;
 };
 
 extern struct HAML_STACK haml_null;
-extern struct HAML_STACK haml_stack;
+/*extern struct HAML_STACK haml_stack;*/
 
-extern struct HAML_STACK stack[HAML_STACK_SIZE];
-
+extern int haml_stack_pointer();
 extern struct HAML_STACK haml_peek();
 extern struct HAML_STACK haml_pop();
 extern void haml_push(struct HAML_STACK stack_element);
 extern int haml_cmp(struct HAML_STACK el1, struct HAML_STACK el2);
 extern void haml_clean(struct HAML_STACK *el);
+extern void haml_deep_copy(struct HAML_STACK *dst, struct HAML_STACK *src);
+typedef void (*stack_op)(struct HAML_STACK *el);
+extern void haml_execute_stack(stack_op f);
 
 #ifdef __cplusplus
 }
