0b48362d0479be90c54e8069e329e4a138cd5893
khr <abusik@hotmail.com>
2010-05-02 11:11:21 -0700

    Added asserts in PushTagNameTest and added MakeTagNameTest.

--- test/gtest/test_helpers.cpp
+++ test/gtest/test_helpers.cpp
@@ -18,6 +18,20 @@ TEST_F(HelpersTests, PushTagNameTest)
   char *indent = (char*)"       ";
   char *tag_name = (char*)"tag_name";
 
+  ASSERT_EQ(-1, haml_stack_pointer());
   push_tag_name(tag_name, indent);
+
+  ASSERT_EQ(0, haml_stack_pointer());
+  struct HAML_STACK expected = { tag_name, indent };
+  ASSERT_EQ(1, haml_cmp(expected, haml_peek()));
 }
 
+TEST_F(HelpersTests, MakeTagNameTest) 
+{
+  char *indent = strdup("       ");
+  char *tag_name = strdup("tag_name");
+
+  ASSERT_EQ(-1, haml_stack_pointer());
+  char* generated_name = make_tag_name(tag_name, indent);
+  free(generated_name);
+}
