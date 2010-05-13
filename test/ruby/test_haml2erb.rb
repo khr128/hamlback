cbc47eb6c78fe563bc876a11b4cb5bc2451c2856
khr <abusik@hotmail.com>
2010-05-12 19:03:45 -0700

    Added test for ps_users/show.

--- test/ruby/test_haml2erb.rb
+++ test/ruby/test_haml2erb.rb
@@ -10,154 +10,88 @@ class TestHaml2erb < Test::Unit::TestCase
 #    File.delete(@output_file_name) if File.exists?(@output_file_name)
   end
 
-  def test_simplest_tag
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest.html.haml")} > #{@output_file_name}`
+  def template_test name
+   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/#{name}.html.haml")} > #{@output_file_name}`
 
-    expected_file_name = File.join(@cur_dir, "expected/simplest.html.erb")
+    expected_file_name = File.join(@cur_dir, "expected/#{name}.html.erb")
     diff = `diff #{@output_file_name} #{expected_file_name}`
     assert_equal 0, $?.exitstatus, diff
     assert diff.empty?, diff
+  end
+
+  def test_simplest_tag
+    template_test "simplest"
   end
 
   def test_simplest_tag_with_id
-    `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_with_id.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_with_id.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_with_id"
   end
 
   def test_simplest_tag_with_id_and_hash
-    `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_with_id_and_hash.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_with_id_and_hash.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_with_id_and_hash"
   end
 
   def test_one_level_nested_tags
-    `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/one_level_nested_tags.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/one_level_nested_tags.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "one_level_nested_tags"
   end
 
   def test_two_level_nested_tags
-    `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/two_level_nested_tags.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/two_level_nested_tags.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "two_level_nested_tags"
   end
 
   def test_simplest_div
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div"
   end
 
   def test_simplest_div_with_static_content
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_static_content.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_static_content.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_static_content"
   end
 
   def test_simplest_div_with_ruby_content
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_ruby_content.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_ruby_content.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_ruby_content"
   end
 
   def test_simplest_div_with_nested_ruby_content
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_nested_ruby_content.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_nested_ruby_content.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_nested_ruby_content"
   end
 
   def test_simplest_div_with_nested_ruby_content_and_continuation
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_nested_ruby_content_and_continuation.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_nested_ruby_content_and_continuation.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_nested_ruby_content_and_continuation"
   end
 
   def test_simplest_div_with_nested_ruby_code
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_nested_ruby_code.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_nested_ruby_code.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_nested_ruby_code"
   end
 
   def test_simplest_div_with_comment
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_comment.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_comment.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_comment"
   end
 
   def test_simplest_div_with_html_comment
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_html_comment.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_html_comment.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_html_comment"
   end
 
   def test_simplest_div_with_plain_text
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_plain_text.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_plain_text.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_plain_text"
   end
 
   def test_simplest_div_with_escaped_plain_text
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/simplest_div_with_escaped_plain_text.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/simplest_div_with_escaped_plain_text.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "simplest_div_with_plain_text"
   end
 
   def test_ps_users_index
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/ps_users/index.html.haml")} > #{@output_file_name}`
-
-    expected_file_name = File.join(@cur_dir, "expected/ps_users/index.html.erb")
-    diff = `diff #{@output_file_name} #{expected_file_name}`
-    assert_equal 0, $?.exitstatus, diff
-    assert diff.empty?, diff
+    template_test "ps_users/index"
   end
 
   def test_ps_users_new
-   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/ps_users/new.html.haml")} > #{@output_file_name}`
+    template_test "ps_users/new"
+  end
 
-    expected_file_name = File.join(@cur_dir, "expected/ps_users/new.html.erb")
+  def test_ps_users_show
+    template_test "ps_users/show"
+   `#{@ha2er_exe} < #{File.join(@cur_dir, "haml/ps_users/show.html.haml")} > #{@output_file_name}`
+
+    expected_file_name = File.join(@cur_dir, "expected/ps_users/show.html.erb")
     diff = `diff #{@output_file_name} #{expected_file_name}`
     assert_equal 0, $?.exitstatus, diff
     assert diff.empty?, diff
